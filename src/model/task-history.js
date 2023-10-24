99341dbb589cf660fe7e668ee1eb74ea8a785892
Arthur Boeck <arthurguterres@outlook.com>
2023-10-24 20:12:30 -0300

    remove models

--- src/model/task-history.js
+++
@@ -1,8 +0,0 @@
-export default class TaskHistory {
-    constructor(idTask, descricao, usuario, data) {
-        this.idTask = idTask;
-        this.descricao = descricao;
-        this.usuario = usuario;
-        this.data = data;
-    }
-};
--- src/model/user.js
+++
@@ -1,8 +0,0 @@
-export default class Task {
-    constructor(nome, usuario, senha, dataCriacao) {
-        this.nome = nome;
-        this.usuario = usuario;
-        this.senha = senha;
-        this.dataCriacao = dataCriacao;
-    }
-};
