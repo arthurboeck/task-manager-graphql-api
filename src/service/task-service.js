e575d21b3066f44d0ccb15929ab984b3b97573fd
Arthur Boeck <arthurguterres@outlook.com>
2023-10-24 18:27:36 -0300

    refactory para funcoes sempre retornar o objeto atualizado

--- src/service/task-service.js
+++ src/service/task-service.js
@@ -3,12 +3,18 @@ import * as taskHistoryService from '../service/task-history-service.js';
 import { getUserByUserName } from '../service/user-service.js';
 import { NotFoundError } from '../infra/error/request-error.js';
 import validateTask from './task-validator.js';
-import taskHistoryStatus from '../enums/task-history-status.js';
+import taskHistoryStatus from '../enum/task-history-status.js';
+import taskStatus from '../enum/task-status.js';
 
 export async function getTasks() {
     const tasks = await taskRepository.getTasks();
     if (!tasks || tasks.length === 0) {
         throw new NotFoundError('Nenhuma tarefa encontrada!');
+    }
+
+    for (const task of tasks) {
+        const history = await taskHistoryService.getTaskHistoryByTaskId(task.id);
+        task.historico = history;
     }
     return tasks;
 };
@@ -18,6 +24,9 @@ export async function getTaskById(taskId) {
     if (!task) {
         throw new NotFoundError('Tarefa não encontrada!');
     }
+
+    const history = await taskHistoryService.getTaskHistoryByTaskId(taskId);
+    task.historico = history;
     return task;
 };
 
@@ -25,8 +34,10 @@ export async function createTask(task, userLogged) {
     try {
         validateTask(task);
         await getUserByUserName(task.responsavel.replace(/\s+/g, ' '));
-        let newTaskId = await taskRepository.insertTask(task);
-        taskHistoryService.createTaskHistory(newTaskId, userLogged, taskHistoryStatus.CRIADA);
+        task.status = taskStatus.PENDENTE;
+        let newTask = await taskRepository.insertTask(task);
+        taskHistoryService.createTaskHistory(newTask.id, userLogged, taskHistoryStatus.CRIADA);
+        return await getTaskById(newTask.id);
     } catch (error) {
         throw error;
     }
@@ -36,21 +47,36 @@ export async function updateTask(taskId, taskUpdate, userLogged) {
     try {
         await taskRepository.getTaskById(taskId);
 
-        validateTask(taskUpdate);
+        validateTask(taskUpdate, true);
         await getUserByUserName(taskUpdate.responsavel.replace(/\s+/g, ' '));
-        taskRepository.updateTask(taskId, taskUpdate);
+        await taskRepository.updateTask(taskId, taskUpdate);
         taskHistoryService.createTaskHistory(taskId, userLogged, taskHistoryStatus.ALTERADA);
+        return await getTaskById(taskId);
+    } catch (error) {
+        throw error;
+    }
+};
+
+export async function completeTask(taskId, userLogged) {
+    try {
+        await taskRepository.completeTask(taskId);
+        await taskHistoryService.createTaskHistory(taskId, userLogged, taskHistoryStatus.CONCLUIDA);
+        return await getTaskById(taskId);
     } catch (error) {
         throw error;
     }
 };
 
 export async function deleteTask(taskId) {
+    let mutationStatus;
     try {
         await taskRepository.getTaskById(taskId);
         await taskHistoryService.deleteTaskHistoryByTaskId(taskId);
         await taskRepository.deleteTask(taskId);
+        mutationStatus = true;
     } catch (error) {
+        mutationStatus = false;
         throw error;
     }
+    return mutationStatus;
 };
