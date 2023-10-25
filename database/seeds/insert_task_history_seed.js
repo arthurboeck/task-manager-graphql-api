fe53ffb4089a89107ac1f055fdd024fababe5b40
Arthur Boeck <arthurguterres@outlook.com>
2023-10-25 20:38:25 -0300

    ajuste default de datas

--- database/seeds/insert_task_history_seed.js
+++ database/seeds/insert_task_history_seed.js
@@ -2,18 +2,18 @@
  * @param {import("../../../../")} knex
  */
 export async function seed(knex) {
-  await knex("task_history").del();
-  await knex("task_history").insert(taskHistory);
-};
+  await knex('task_history').del();
+  await knex('task_history').insert(taskHistory);
+}
 
 const data = new Date();
 
 const taskHistory = [
-  { id: 1, idTask: 1, descricao: "TAREFA CRIADA", usuario: "boeckarthur", data: data.toISOString() },
+  {id: 1, idTask: 1, descricao: 'TAREFA CRIADA', usuario: 'boeckarthur', data: data.toISOString()},
 
-  { id: 2, idTask: 2, descricao: "TAREFA CRIADA", usuario: "arthur_guterres", data: data.toISOString() },
-  { id: 3, idTask: 2, descricao: "TAREFA CONCLUIDA", usuario: "arthur_guterres", data: data.toISOString() },
+  {id: 2, idTask: 2, descricao: 'TAREFA CRIADA', usuario: 'arthur_guterres', data: data.toISOString()},
+  {id: 3, idTask: 2, descricao: 'TAREFA CONCLUIDA', usuario: 'arthur_guterres', data: data.toISOString()},
 
-  { id: 4, idTask: 3, descricao: "TAREFA CRIADA", usuario: "arthur_guterres", data: data.toISOString() },
-  { id: 5, idTask: 3, descricao: "TAREFA CANCELADA", usuario: "arthur_guterres", data: data.toISOString() },
-]
+  {id: 4, idTask: 3, descricao: 'TAREFA CRIADA', usuario: 'arthur_guterres', data: data.toISOString()},
+  {id: 5, idTask: 3, descricao: 'TAREFA CANCELADA', usuario: 'arthur_guterres', data: data.toISOString()},
+];
--- database/seeds/insert_user_seed.js
+++ database/seeds/insert_user_seed.js
@@ -2,12 +2,12 @@
  * @param {import("../../../../")} knex
  */
 export async function seed(knex) {
-  await knex("user").del();
-  await knex("user").insert(users);
-};
+  await knex('user').del();
+  await knex('user').insert(users);
+}
 
 const users = [
-  { id: 1, nome: "Arthur Guterres", usuario: "arthur_guterres", senha: "" },
-  { id: 2, nome: "Arthur Boeck", usuario: "boeckarthur", senha: "" },
-  { id: 3, nome: "Getulio Vargas", usuario: "getulinho", senha: "" }
+  {id: 1, nome: 'Arthur Guterres', usuario: 'arthur_guterres', senha: ''},
+  {id: 2, nome: 'Arthur Boeck', usuario: 'boeckarthur', senha: ''},
+  {id: 3, nome: 'Getulio Vargas', usuario: 'getulinho', senha: ''},
 ];
